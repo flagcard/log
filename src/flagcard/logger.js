820c7deecddaf6d74dae18bbdd38d587feb9dcd1
Dayvison Barros Lemos <dayvison.lemos@gmail.com>
2019-09-26 20:48:52 -0300

    Correção da estratégia de log

--- src/flagcard/logger.js
+++ src/flagcard/logger.js
@@ -1,26 +1,30 @@
-const apm = require('elastic-apm-node');
 const moment = require('moment-timezone');
 const winston = require('winston');
+require('winston-daily-rotate-file');
 
 const timezone = 'America/Manaus';
 
-const start = (serviceName) => {
-  const options = {
-    serviceName,
-    serverUrl: 'http://10.136.211.90:9202',
-  };
-  apm.start(options);
-};
-
 const format = winston.format.printf(({ level, message, timestamp }) => {
-  const localDate = moment(timestamp).tz(timezone).format('YYYY-MM-SS HH:mm:ss');
-  return `[${level.toUpperCase()}]-[${localDate}}]: ${message}\n`;
+  const localDate = moment(timestamp).tz(timezone).format('HH:mm:ss');
+  return `[${level.toUpperCase()}-${localDate}]: ${message}`;
 });
 
+const transports = [];
 
-const transport = new winston.transports.Console({
+transports.push(new winston.transports.Console({
+  colorize: true,
   format,
-});
+}));
+
+if (process.env.NODE_ENV === 'production') {
+  transports.push(new (winston.transports.DailyRotateFile)({
+    filename: 'logs/application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+  }));
+}
 
 const logger = winston.createLogger({
   level: 'info',
@@ -28,30 +32,9 @@ const logger = winston.createLogger({
     winston.format.timestamp(),
     format,
   ),
-  transports: [transport],
+  transports,
 });
 
-const errorLogMiddleware = (err, req, res, next) => {
-  let error;
-  try {
-    if (err.message && Object.keys(err.message).length) {
-      error = JSON.stringify(err.message);
-    } else if (err.stack && Object.keys(err.stack).length) {
-      error = JSON.stringify(err.stack);
-    } else if (err.data && Object.keys(err.data).length) {
-      error = JSON.stringify(err.data);
-    } else {
-      error = JSON.stringify(err);
-    }
-  } catch (e) {
-    error = 'Não foi possível decodificar o erro.';
-  }
-  logger.error(`something went wrong in asgard: ${error}`);
-  next(err);
-};
-
 module.exports = {
-  start,
   log: logger,
-  errorLogMiddleware,
 };
--- test/logger/middleware.test.js
+++
@@ -1,7 +0,0 @@
-const { errorLogMiddleware } = require('../../src/flagcard/logger');
-
-describe('Midleware', () => {
-  it('Log format structure for error level', () => {
-    errorLogMiddleware({ err: 'error message' }, null, null, () => {});
-  });
-});
