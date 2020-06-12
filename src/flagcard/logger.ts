a59b999ccb1e69a62f7cbbc6ba560e9ca1c33054
Dayvison Barros Lemos <dayvison.lemos@gmail.com>
2020-06-12 20:56:55 -0300

    Correção de Lint

--- src/flagcard/logger.ts
+++ src/flagcard/logger.ts
@@ -5,7 +5,9 @@ import { formatToTimeZone } from 'date-fns-timezone';
 const timeZone = 'America/Manaus';
 
 const format = winston.format.printf(({ level, message, timestamp }) => {
-  const localDate = formatToTimeZone(timestamp, 'MMM DD, YYYY @ HH:mm:ss', { timeZone });
+  const localDate = formatToTimeZone(timestamp, 'MMM DD, YYYY @ HH:mm:ss', {
+    timeZone,
+  });
   return `[${localDate}]-[${level.toLowerCase()}]: ${message}`;
 });
 
@@ -17,13 +19,15 @@ transports.push(
   }),
 );
 
-if(process.env.FLAGCARD_WRITE_LOG){
-  transports.push(new (winston.transports.DailyRotateFile)({
-    filename: 'logs/application-%DATE%.log',
-    datePattern: 'YYYYMMDDHH',
-    maxSize: '1m',
-    maxFiles: '2d',
-  }));
+if (process.env.FLAGCARD_WRITE_LOG) {
+  transports.push(
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYYMMDDHH',
+      maxSize: '1m',
+      maxFiles: '2d',
+    }),
+  );
 }
 
 export default winston.createLogger({
