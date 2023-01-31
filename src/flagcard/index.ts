f6db09d7ab0b948876fceebbe1b9567db189fcdb
mikecodejs <mikerxvxl@gmail.com>
2023-01-31 14:36:01 -0400

    feat: update architecture

--- src/flagcard/index.ts
+++ src/flagcard/index.ts
@@ -1,11 +1,34 @@
-import loggerWithoutTracer from './logger';
-import loggerTracer from './logger-tracer';
+import { formatToTimeZone } from 'date-fns-timezone';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
 
-const logger = {
-  options: {
-    loggerTracer,
-    loggerWithoutTracer,
-  },
-};
+const timeZone = 'America/Manaus';
 
-export default logger;
+const format = winston.format.printf(({ level, message, timestamp }) => {
+  const localDate = formatToTimeZone(timestamp, 'MMM DD, YYYY @ HH:mm:ss', {
+    timeZone,
+  });
+
+  return `[${localDate}]-[${level.toLowerCase()}]: ${message}`;
+});
+
+const transports = [];
+
+transports.push(new winston.transports.Console({ format }));
+
+if (process.env.FLAGCARD_WRITE_LOG) {
+  transports.push(
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYYMMDDHH',
+      maxSize: '1m',
+      maxFiles: '2d',
+    }),
+  );
+}
+
+export default winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(winston.format.timestamp(), format),
+  transports,
+});
